15bb6fc4a16da271621a3423a4f9a721f1c751fc
Sasha <78263390+hyperpugs@users.noreply.github.com>
2024-11-21 13:25:05 +0300

    links + firefox

---
+++ links.py
@@ -0,0 +1,3 @@
+MAIN_URL = 'https://qa-scooter.praktikum-services.ru/' # базовая страница яндекс.самокат
+ORDER_URL = f"{MAIN_URL}/order" # страница для заказа самоката
+DZEN_URL = "https://dzen.ru/?yredirect=true" # страница яндекс.дзена
